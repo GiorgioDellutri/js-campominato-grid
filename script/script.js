0d28a3eac2f231dde2cc7dafffaba9b459b0e9c0
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-30 17:54:11 +0100

    index square

--- script/script.js
+++ script/script.js
@@ -5,14 +5,33 @@ const wrapperContainer = document.querySelector('div.grid-container');
 
 // CREO IL QUADRATO 100 VOLTE
 
-for (let i = 0; i < 100; i++){
-    // CREO IL DIV
-const newSquare = document.createElement('div');
+for (let i = 1; i <= 100; i++){
 
-// ATTRIBUISCO LE PROPRIETA CHE VOLUTE
-newSquare.classList.add('square');
+    const newSquare = squareCreation(i);
 
-// AGGIUNGO IL DIV AL PARENT
-wrapperContainer.appendChild(newSquare);
+    // AGGIUNGO IL DIV AL PARENT
+    wrapperContainer.appendChild(newSquare);
 
 }
+
+// FUNZIONE PER CREARE IL QUADRATO
+
+function squareCreation (content){
+    // CREO IL DIV
+    const newSquare = document.createElement('div');
+
+    // ATTRIBUISCO LE PROPRIETA CHE VOLUTE
+    newSquare.classList.add('square', 'd-flex');
+    
+    // AGGIUNGO IL NUMERO DELLA CASELLA
+    newSquare.innerHTML = `<span class="m-auto"> ${content} </span>`;
+
+    // TRA CUI UN COMPORTAMENTO AL CLICK
+    newSquare.addEventListener("click", function(){
+
+        // aggiungo la classe per colorarlo di azzurro
+        newSquare.classList.toggle('clicked');
+    });
+
+    return newSquare;
+} 
