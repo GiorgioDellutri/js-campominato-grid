73a0f52e1b97e5d553c5fdd81bf5ea143de94080
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-30 18:24:37 +0100

    play button

--- script/script.js
+++ script/script.js
@@ -1,37 +1,47 @@
+// RECUPERO IL TASTO PLAY DALLA SUA CLASSE
+let play = document.querySelector('div.play');
 
+// AGGIUNGO AL BOTTONE PLAY UN EVENTO. ESSO CONTERRA' TUTTO L'ALGORITMO
+play.addEventListener('click', function(){
+    // RECUPERO IL PARENT
+    const wrapperContainer = document.querySelector('div.grid-container');
 
-// RECUPERO IL PARENT
-const wrapperContainer = document.querySelector('div.grid-container');
-
-// CREO IL QUADRATO 100 VOLTE
-
-for (let i = 1; i <= 100; i++){
-
-    const newSquare = squareCreation(i);
-
-    // AGGIUNGO IL DIV AL PARENT
-    wrapperContainer.appendChild(newSquare);
-
-}
-
-// FUNZIONE PER CREARE IL QUADRATO
-
-function squareCreation (content){
-    // CREO IL DIV
-    const newSquare = document.createElement('div');
-
-    // ATTRIBUISCO LE PROPRIETA CHE VOLUTE
-    newSquare.classList.add('square', 'd-flex');
+    // INIZIALIZZO LA GRIGLIA
+    wrapperContainer.innerHTML= "";
     
-    // AGGIUNGO IL NUMERO DELLA CASELLA
-    newSquare.innerHTML = `<span class="m-auto"> ${content} </span>`;
+    // CREO IL QUADRATO 100 VOLTE
+    
+    for (let i = 1; i <= 100; i++){
+    
+        const newSquare = squareCreation(i);
+    
+        // AGGIUNGO IL DIV AL PARENT
+        wrapperContainer.appendChild(newSquare);
+    
+    }
+    
+    // FUNZIONE PER CREARE IL QUADRATO
+    
+    function squareCreation (content){
+        // CREO IL DIV
+        const newSquare = document.createElement('div');
+    
+        // ATTRIBUISCO LE PROPRIETA CHE VOLUTE
+        newSquare.classList.add('square', 'd-flex');
+        
+        // AGGIUNGO IL NUMERO DELLA CASELLA
+        newSquare.innerHTML = `<span class="m-auto"> ${content} </span>`;
+    
+        // TRA CUI UN COMPORTAMENTO AL CLICK
+        newSquare.addEventListener("click", function(){
+    
+            // aggiungo la classe per colorarlo di azzurro
+            newSquare.classList.toggle('clicked');
+        });
+    
+        return newSquare;
+    } 
 
-    // TRA CUI UN COMPORTAMENTO AL CLICK
-    newSquare.addEventListener("click", function(){
+})
 
-        // aggiungo la classe per colorarlo di azzurro
-        newSquare.classList.toggle('clicked');
-    });
 
-    return newSquare;
-} 
