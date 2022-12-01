e6b7488641396916e8aefb5238fbd12d90c24614
GiorgioDellutri <giorgiodll93@gmail.com>
2022-12-01 10:46:50 +0100

    add once : true play event listener

--- script/script.js
+++ script/script.js
@@ -19,33 +19,31 @@ play.addEventListener('click', function(){
     
         // AGGIUNGO IL DIV AL PARENT
         wrapperContainer.appendChild(newSquare);
-    
     }
     
-    // FUNZIONE PER CREARE IL QUADRATO
-    
-    function squareCreation (content){
-        // CREO IL DIV
-        const newSquare = document.createElement('div');
-    
-        // ATTRIBUISCO LE PROPRIETA CHE VOLUTE
-        newSquare.classList.add('square', 'd-flex');
-        
-        // AGGIUNGO IL NUMERO DELLA CASELLA
-        newSquare.innerHTML = `<span class="m-auto"> ${content} </span>`;
-    
-        // TRA CUI UN COMPORTAMENTO AL CLICK
-        newSquare.addEventListener("click", function(){
-    
-            // aggiungo la classe per colorarlo di azzurro
-            newSquare.classList.toggle('clicked');
-        });
-    
-        return newSquare;
-    } 
+}, {once : true});
 
-})
+// FUNZIONE PER CREARE IL QUADRATO
 
+function squareCreation (content){
+    // CREO IL DIV
+    const newSquare = document.createElement('div');
+
+    // ATTRIBUISCO LE PROPRIETA CHE VOLUTE
+    newSquare.classList.add('square', 'd-flex');
+    
+    // AGGIUNGO IL NUMERO DELLA CASELLA
+    newSquare.innerHTML = `<span class="m-auto"> ${content} </span>`;
+
+    // TRA CUI UN COMPORTAMENTO AL CLICK
+    newSquare.addEventListener("click", function(){
+
+        // aggiungo la classe per colorarlo di azzurro
+        newSquare.classList.toggle('clicked');
+    });
+
+    return newSquare;
+} 
 
 function squareIndex (index){
     for(let i = 1; i <= 100; i++){
